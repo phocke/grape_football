4755cec293934b23320f29011ad4a98395d0addc
Przemek Hocke <phocke@gmail.com>
2012-11-20 23:53:45 +0100

    game states and game states spec, included active_support for delegation

--- app/game.rb
+++ app/game.rb
@@ -1,44 +1,57 @@
-require 'state_machine'
-class Game
-  attr_accessor :state
+  require 'state_machine'
+  require 'active_support/core_ext'
+  class Game
+    attr_accessor :state, :machine
+
+    def initialize
+      @state = 'setting_up'
+      machine
+    end
+    delegate :start, :finish, :next_round, :to => :@machine
 
 
-  state_machine :initial => :setting_up do
 
-  end
-
-  def transitions
-    [{:setting_up => :round_1, :on => :start}]
-  end
-
-  # Create a state machine for this vehicle instance dynamically based on the transitions defined from the source above
-  def machine
-    game = self
-    @machine ||= Machine.new(game, :initial => :parked) do
-      game.transitions.each {|attrs| transition(attrs)}
-
-      # Persist the state on the vehicle itself
-      after_transition do
-        game.state = vehicle.machine.state
-        game.save
+    def transitions
+      transitions_array = [{:setting_up => :round_1, :on => :start}, {:round_5 => :finished, :on => :finish}]
+      round_transitions = {}
+      4.times do |n|
+        from, to  = "round_#{n+1}".to_sym, "round_#{n+2}".to_sym
+        round_transitions[from] = to
       end
+      round_transitions[:on] = :next_round
+      transitions_array << round_transitions
+
+      return transitions_array
+    end
+
+    # Create a state machine for this vehicle instance dynamically based on the transitions defined from the source above
+    def machine
+      game = self
+      @machine ||= Machine.new(game, :initial => :setting_up) do
+        game.transitions.each {|attrs| transition(attrs)}
+
+        # Persist the state on the vehicle itself
+        after_transition do
+          game.state = game.machine.state
+          game.save
+        end
+      end
+    end
+
+    def save
+      # Save the state change...
     end
   end
 
-  def save
-    # Save the state change...
-  end
-end
 
-
-class Machine
-  def self.new(object, *args, &block)
-    machine = Class.new do
-      def definition
-        self.class.state_machine
+  class Machine
+    def self.new(object, *args, &block)
+      machine = Class.new do
+        def definition
+          self.class.state_machine
+        end
       end
+      machine.state_machine(*args, &block)
+      machine.new
     end
-    machine.state_machine(*args, &block)
-    machine.new
   end
-end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -7,10 +7,40 @@ describe Game do
 
   it "should be a started game" do
     @game.should be_kind_of(Game)
-    @game.state.should == "setting_up"
   end
 
-  describe "goal?" do
+  describe "states" do
+    it "should intialize in setting_up state" do
+      @game.state.should == "setting_up"
+    end
 
+    it "should go to first round when started" do
+      @game.machine.start
+      @game.state.should == "round_1"
+    end
+
+    it "should have 5 rounds" do
+      @game.start
+      @game.state.should == "round_1"
+
+      4.times do |n|
+        @game.next_round
+        @game.state.should == "round_#{n+2}"
+      end
+    end
+
+    it "should finish after 5 rounds" do
+      @game.start
+
+      4.times do |n|
+        @game.next_round
+      end
+      @game.state.should == "round_5"
+
+      @game.next_round.should be_false #as we didn't move ahead
+
+      @game.finish
+      @game.state.should == "finished"
+    end
   end
 end
