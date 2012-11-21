186a9c2d4a53026602f0435ffe84ed7245297dc9
Przemek Hocke <phocke@gmail.com>
2012-11-21 20:32:24 +0100

    refactor round again so that it gives ints rathed than bools

--- app/round.rb
+++ app/round.rb
@@ -1,16 +1,24 @@
 #This class just checks x and y coordinates - we don't care about being out of bounds here etc.
 class Round
-  attr_accessor :player_1, :player_2, :player_1_scored, :player_2_scored
+  attr_accessor :player_1, :player_2, :player_1_score, :player_2_score
   def initialize(player_1, player_2)
     @player_1 = player_1
     @player_2 = player_2
   end
 
-  def player_1_scored?(shoot_coordinates=@player_1.shoot, save_coordinates=@player_2.save)
-    @player_1_scored.nil? ? @player_1_scored = (save_coordinates != shoot_coordinates) : @player_1_scored
+  def player_1_score(shoot_coordinates=@player_1.shoot, save_coordinates=@player_2.save)
+    if @player_1_score.nil?
+      @player_1_score = (save_coordinates != shoot_coordinates ? 1 : 0)
+    else
+      @player_1_score
+    end
   end
 
-  def player_2_scored?(shoot_coordinates=@player_2.shoot, save_coordinates=@player_1.save)
-    @player_2_scored.nil? ? @player_2_scored = (save_coordinates != shoot_coordinates) : @player_2_scored
+  def player_2_score(shoot_coordinates=@player_2.shoot, save_coordinates=@player_1.save)
+    if @player_2_score.nil?
+      @player_2_score = (save_coordinates != shoot_coordinates ? 1 : 0)
+    else
+      @player_2_score
+    end
   end
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -13,19 +13,19 @@ describe Round do
 
   describe "goal?" do
     it "should cache result" do
-      @round.player_1_scored?([1,2], [1,3]).should be_true
-      @round.player_1_scored?([1,2], [1,2]).should be_true #as we don't want to change it after shooting is done
+      @round.player_1_score([1,2], [1,3]).should == 1
+      @round.player_1_score([1,2], [1,2]).should == 1 #as we don't want to change it after shooting is done
 
-      @round.player_2_scored?([1,2], [1,2]).should be_false
-      @round.player_2_scored?([1,2], [1,22]).should be_false
+      @round.player_2_score([1,2], [1,2]).should == 0
+      @round.player_2_score([1,2], [1,22]).should == 0
     end
 
     it "should be true if there's a goal" do
-      @round.player_1_scored?([1,2], [1,3]).should be_true
+      @round.player_1_score([1,2], [1,3]).should == 1
     end
 
     it "should be false if there's no goal" do
-      @round.player_1_scored?([1,2], [1,2]).should be_false
+      @round.player_1_score([1,2], [1,2]).should == 0
     end
   end
 end
