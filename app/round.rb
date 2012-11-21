955736891b7ddc1f4a6306cc512ed7d4b8ec884a
Przemek Hocke <phocke@gmail.com>
2012-11-21 19:48:42 +0100

    refactor round

--- app/round.rb
+++ app/round.rb
@@ -1,12 +1,16 @@
 #This class just checks x and y coordinates - we don't care about being out of bounds here etc.
 class Round
-  attr_accessor :shoot_coordinates, :save_coordinates
-  def initialize(shoot_coordinates=[], save_coordinates=[])
-    @shoot_coordinates = shoot_coordinates
-    @save_coordinates = save_coordinates
+  attr_accessor :player_1, :player_2, :player_1_scored, :player_2_scored
+  def initialize(player_1, player_2)
+    @player_1 = player_1
+    @player_2 = player_2
   end
 
-  def goal?
-    @save_coordinates != @shoot_coordinates
+  def player_1_scored?(shoot_coordinates=@player_1.shoot, save_coordinates=@player_2.save)
+    @player_1_scored.nil? ? @player_1_scored = (save_coordinates != shoot_coordinates) : @player_1_scored
+  end
+
+  def player_2_scored?(shoot_coordinates=@player_2.shoot, save_coordinates=@player_1.save)
+    @player_2_scored.nil? ? @player_2_scored = (save_coordinates != shoot_coordinates) : @player_2_scored
   end
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -2,7 +2,9 @@ require 'spec_helper'
 
 describe Round do
   before(:each) do
-    @round = Round.new
+    player_1 = Player.new
+    player_2 = Player.new
+    @round = Round.new(player_1, player_2)
   end
 
   it "should be a round" do
@@ -10,32 +12,20 @@ describe Round do
   end
 
   describe "goal?" do
+    it "should cache result" do
+      @round.player_1_scored?([1,2], [1,3]).should be_true
+      @round.player_1_scored?([1,2], [1,2]).should be_true #as we don't want to change it after shooting is done
+
+      @round.player_2_scored?([1,2], [1,2]).should be_false
+      @round.player_2_scored?([1,2], [1,22]).should be_false
+    end
+
     it "should be true if there's a goal" do
-      @round.shoot_coordinates = [0,0]
-      @round.save_coordinates = [0,3]
-      @round.goal?.should be_true
-
-      @round.shoot_coordinates = [1,4]
-      @round.save_coordinates = [1,1]
-      @round.goal?.should be_true
-
-      @round.shoot_coordinates = [-1,-4]
-      @round.save_coordinates = [1,1]
-      @round.goal?.should be_true
+      @round.player_1_scored?([1,2], [1,3]).should be_true
     end
 
     it "should be false if there's no goal" do
-      @round.shoot_coordinates = [0,0]
-      @round.save_coordinates = [0,0]
-      @round.goal?.should be_false
-
-      @round.shoot_coordinates = [1,4]
-      @round.save_coordinates = [1,4]
-      @round.goal?.should be_false
-
-      @round.shoot_coordinates = [1,-4]
-      @round.save_coordinates = [1,-4]
-      @round.goal?.should be_false
+      @round.player_1_scored?([1,2], [1,2]).should be_false
     end
   end
 end
